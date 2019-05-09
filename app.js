6c07d519c194d0bf531d0e00fff06b101f448b76
Benedictiknkeonye@gmail.com <Benedictiknkeonye@gmail.com>
2019-05-09 19:32:45 +0100

    Created the first method  for the library - GET

--- app.js
+++ app.js
@@ -2,5 +2,9 @@ const http = new eezyHTTP();
 
 // GET Posts
 http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
-  console.log(posts);
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(posts);
+  }
 });
